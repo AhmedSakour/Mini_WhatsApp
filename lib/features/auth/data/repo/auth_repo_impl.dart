9844eb19a2ce888309b26fedd91bcb2a93029ca1
ahmad_sakour <hussian0938605821@gmail.com>
2024-09-26 12:28:46 +0300

    add some files to core and auth feature

---
+++ lib/features/auth/data/repo/auth_repo_impl.dart
@@ -0,0 +1,3 @@
+import 'package:whats_app/features/auth/data/repo/auht_repo.dart';
+
+class AuthRepoImpl extends AuhtRepo {}
---
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:whats_app/features/auth/presentation/views/widget/sign_up_view_body.dart';
+
+class SignUpView extends StatelessWidget {
+  const SignUpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SignUpViewBody();
+  }
+}
