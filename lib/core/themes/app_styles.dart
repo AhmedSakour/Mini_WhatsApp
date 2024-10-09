5bc12ac4eb3ead3484d0e734ea1782805a3ff767
ahmad_sakour <hussian0938605821@gmail.com>
2024-10-09 13:20:08 +0300

    add colors to color file

--- lib/core/themes/app_styles.dart
+++ lib/core/themes/app_styles.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:whats_app/core/themes/colors.dart';
 
 abstract class AppStyles {
   static TextStyle styleBold30 = const TextStyle(
       color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30);
 
-  static TextStyle styleMedium20 = const TextStyle(
-      color: Color(0xff2B8A86), fontWeight: FontWeight.w500, fontSize: 20);
+  static TextStyle styleMedium20 = TextStyle(
+      color: AppColor.primaryColor, fontWeight: FontWeight.w500, fontSize: 20);
 }
