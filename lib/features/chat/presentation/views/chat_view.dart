6420dbf83cab241d12a34c480a6ea435f98e1fc4
ahmad_sakour <hussian0938605821@gmail.com>
2024-10-19 18:53:59 +0300

    create floatingActionButton for chat feature

--- lib/features/chat/presentation/views/chat_view.dart
+++ lib/features/chat/presentation/views/chat_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whats_app/features/chat/presentation/views/widgets/chat_view_body.dart';
+import 'package:whats_app/features/chat/presentation/views/widgets/custom_floating_action_button.dart';
 
 import '../../../../core/shared_widget/custom_appBar.dart';
 
@@ -11,6 +12,7 @@ class ChatView extends StatelessWidget {
     return const Scaffold(
       appBar: CustomAppBar(),
       body: ChatViewBody(),
+      floatingActionButton: CustomFloatingActionButton(),
     );
   }
 }
