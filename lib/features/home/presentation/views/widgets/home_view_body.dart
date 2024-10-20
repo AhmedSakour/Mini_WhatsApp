e7654ba95000e91d3916c8ebd250e2a48e1777cd
ahmad_sakour <hussian0938605821@gmail.com>
2024-10-20 12:34:53 +0300

    rename chat feature to home

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'chat_list_view.dart';
 
-class ChatViewBody extends StatelessWidget {
-  const ChatViewBody({super.key});
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whats_app/core/themes/app_theme.dart';
 import 'package:whats_app/features/auth/presentation/views/sign_up_view.dart';
-import 'package:whats_app/features/chat/presentation/views/chat_view.dart';
+import 'package:whats_app/features/home/presentation/views/home_view.dart';
 
 import 'features/app_layout/presentation/view/app_layout_view.dart';
 import 'features/auth/presentation/views/login_view.dart';
-import 'features/home/presentation/views/home_view.dart';
 import 'features/splash/presentation/view/splash_view.dart';
 import 'firebase_options.dart';
 
@@ -38,11 +37,10 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const AppLayoutView(),
-        '/home': (context) => const HomeView(),
         '/splash': (context) => const SplashView(),
         '/login': (context) => const LoginView(),
         '/register': (context) => const SignUpView(),
-        '/chat': (context) => const ChatView(),
+        '/home': (context) => const HomeView(),
       },
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
