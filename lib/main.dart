9718dead8929f054bb645ba947b66f9f93c9eac3
ahmad_sakour <hussian0938605821@gmail.com>
2024-11-09 14:16:35 +0300

    pubget in chat

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:whats_app/features/auth/presentation/views/sign_up_view.dart';
 import 'package:whats_app/features/home/presentation/views/bottom_navigation_bar_view.dart';
 import 'package:whats_app/features/home/presentation/views/home_view.dart';
 
+//import 'package:whats_app/core/functions/service_locator.dart';
+
 import 'features/app_layout/presentation/view/app_layout_view.dart';
 import 'features/auth/presentation/views/login_view.dart';
 import 'features/splash/presentation/view/splash_view.dart';
@@ -14,7 +16,6 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  //I Hide firebase options file in gitignore
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -31,7 +32,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       theme: AppTheme.lightTheme(context),
